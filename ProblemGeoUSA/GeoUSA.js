0ea6b4f9e50a8de1a4c66d31976849a87826749e
brandontineo <brandontineo@college.harvard.edu>
2014-04-23 00:37:15 -0400

    add

---
+++ ProblemGeoUSA/GeoUSA.js
@@ -0,0 +1,291 @@
+var margin = {
+    top: 20,
+    right: 50,
+    bottom: 100,
+    left: 100
+};
+
+var width = 1660 - margin.left - margin.right;
+var height = 700 - margin.bottom - margin.top;
+var centered;
+
+var bbVis = {
+    x: 100,
+    y: 10,
+    w: width - 100,
+    h: 300
+};
+
+
+var canvas = d3.select("#vis").append("svg").attr({
+    width: 1300,
+    height: height + margin.top + margin.bottom
+    })
+
+var svg = canvas.append("g").attr({
+        transform: "translate(" + margin.left + "," + (margin.top - 40) + ")"
+    });
+
+var projection = d3.geo.albersUsa().translate([width / 2, height / 2]);//.precision(.1);
+var path = d3.geo.path().projection(projection);
+
+
+
+function loadStations() {
+    d3.csv("../data/NSRDB_StationsMeta.csv",function(error,data){
+
+
+          var tooltip2 = d3.select("body")
+          .append("div")
+          .style("position", "absolute")
+          .style("z-index", "10")
+          .style("visibility", "visible")
+          .style("color", "black")
+          .style("font-size", "15px")
+          .style("top", "450px")
+          .style("left", "15px")
+          .attr("class", "tooltip2")
+          .html("<h2> Map Data: </h2><ul> <li> The USA map was created using a TopoJSON file.</li> <li> Station data was read in and parsed from a CSV file </li> <li> Solar radiation information was data wrangled from the NSRDB database into a compressed JavaScript object. </li></ul> <h2> Interactive USA Map Features: </h2><ul> <li> On click zoom of map </li> <li> Mouseover tooltips with station information </li> <li> On click detailed visualization of station (bar chart) </li> <li> On zoom and on mouseover highlighting of state  </li> <li> Station circles sized proportional to solar radiation </li> <li>Created with JavaScript (d3 library), HTML, and CSS </li></ul>");
+
+    all_array = [];
+    for (i in completeDataSet)
+    {
+     all_array.push(completeDataSet[i]['sum'])  
+       
+    }
+
+    var max = all_array.sort(function(a,b){return b-a;})[0];
+
+
+    var radiusscale = d3.scale.linear().domain([0,max]).range([1, 8]);  
+
+
+    data.forEach(function (d,i) {
+
+    if ((d['USAF'] != "785140") && (d['USAF'] !=  "785145")
+
+      && (d['USAF'] != "785203") && (d['USAF'] != "785260")  
+      && (d['USAF'] != "785263")  && (d['USAF'] != "785350") 
+      && (d['USAF'] != "785430")  && (d['USAF'] != "910660")
+      && (d['USAF'] != "912120") && (d['USAF'] != "912170") 
+      && (d['USAF'] != "912180"))
+    {
+    if (completeDataSet[d['USAF']] == undefined)
+    {
+      var latitude = +d['NSRDB_LAT (dd)'];
+      var longitude = +d['NSRDB_LON(dd)']; //console.log(latitude); console.log(longitude);
+      var screencoord = projection([longitude, latitude]); //console.log(screencoord);
+      svg.append("circle")
+          .attr("cx", screencoord[0]) // projected location on map (pixel)
+          .attr("cy", screencoord[1])
+          .attr("fill", "blue")
+          .attr("r", 2);
+    }
+    else
+    {
+
+      var agg_val =  completeDataSet[d['USAF']]['sum'];
+      var new_rad =  radiusscale(completeDataSet[d['USAF']]['sum']);
+      var stations_name = d['STATION'];
+      var tooltip = d3.select("body")
+          .append("div")
+          .style("position", "absolute")
+          .style("z-index", "10")
+          .style("visibility", "hidden")
+          .style("color", "black")
+          .style("font-size", "15px")
+          .attr("class", "tooltip")
+          .text("Aggregated value for " + stations_name + " is: " + agg_val);    
+
+      var latitude = +d['NSRDB_LAT (dd)'];
+      var longitude = +d['NSRDB_LON(dd)']; //console.log(latitude); console.log(longitude);
+      var screencoord = projection([longitude, latitude]); //console.log(screencoord);
+      svg.append("circle")
+          .attr("cx", screencoord[0]) // projected location on map (pixel)
+          .attr("cy", screencoord[1])
+          .on("mouseover", function(){return tooltip.style("visibility", "visible");})
+          .on("mousemove", function(){return tooltip.style("top", (event.pageY-20)+"px").style("left",(event.pageX+20)+"px");})
+          .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
+          .attr("r", new_rad)
+          .on("click", function()
+          {
+
+          updateDetailVis();
+
+
+        thisid = d['USAF'];
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, 300], .1)
+            .domain([0, bbVis.w]);
+
+        var y = d3.scale.linear()
+            .domain([bbVis.h/2, 0])
+            .range([300, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(12);
+
+
+        for (i in completeDataSet)
+        {
+          all_array.push(completeDataSet[i]['sum'])  
+         
+        }
+
+        var max = all_array.sort(function(a,b){return b-a;})[0];
+
+
+
+
+        var hours = completeDataSet[thisid]['hourly'];
+        array_of_vals = [];
+        array_of_hours = [];
+
+
+        for (l in hours)
+        {
+          array_of_vals.push(hours[l]);
+          array_of_hours.push(l);
+
+        }
+
+          x.domain(array_of_hours);
+
+
+          y.domain([0, 18000000]);
+
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + bbVis.h + ")")
+              //.attr("class", "thedetail")
+              .attr("class", "axis")
+              .call(xAxis)
+              .attr("class", "thedetail")
+              .selectAll("text") 
+              .style("text-anchor", "end")
+              .attr("dx", "-.8em")
+              .attr("dy", ".15em")
+              .attr("transform", function(d) {
+                  return "rotate(-65)" 
+                  })
+              .attr("class", "thedetail")
+              ;
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .attr("class", "thedetail")
+              .attr("class", "axis")
+              .call(yAxis)
+              .attr("class", "thedetail")
+              .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Values")
+              .attr("class", "thedetail")
+              .attr("x", -60);
+
+
+
+          svg.selectAll(".bar")
+              .data(array_of_vals)
+              .enter().append("rect")
+              .attr("class", "bar")
+              .attr("class", "thedetail")
+              .attr("x", function (d,i) { return x(array_of_hours[i]); })
+              .attr("width", x.rangeBand())
+              .attr("y", function(d) { console.log(d); return y(d); })
+              .attr("height", function(d) { return bbVis.h - y(d); });
+
+        var yTextPadding = 20;
+
+        d3.select("g")
+          .append("text")
+          .style("color", "black")
+          .style("font-size", "15px")
+          .attr("class", "thedetail")
+          .attr("x", 80)
+          .attr("y", 20)
+          .text(stations_name);
+
+
+});
+
+          }}})});}
+
+
+
+function loadStats() {
+
+    d3.json("../data/reducedMonthStationHour2003_2004.json", function(error,data){
+    completeDataSet= data;
+    all_array = [];	   
+    for (i in completeDataSet)
+    {
+     all_array.push(completeDataSet[i]['sum'])  
+       
+    }
+  loadStations();
+
+    })}
+
+
+d3.json("../data/us-named.json", function(error, data) {
+
+  var usMap = topojson.feature(data,data.objects.states).features
+
+  svg.selectAll(".country").data(usMap).enter().append("path")
+      .attr("d", path).attr("classed", "country").on("click", zooming);
+
+  svg.append("path")
+      .datum(topojson.mesh(data, data.objects.states, function(a, b) { return a !== b; }))
+      .attr("id", "state-borders")
+      .attr("d", path);
+
+    loadStats();
+});
+
+
+
+function updateDetailVis() 
+{
+  d3.selectAll(".thedetail").remove();
+}
+  
+
+
+function zooming(d) {
+  var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 0.9;
+    centered = null;
+  }
+
+  svg.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+
+  svg.transition()
+      .duration(750)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
+}
+
+
---
+++ ProblemGeoWorld/GeoWorld.js
@@ -0,0 +1,323 @@
+/**
+ * Created by hen on 3/8/14.
+ */
+
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+var width = 960 - margin.left - margin.right;
+var height = 700 - margin.bottom - margin.top;
+
+
+
+var bbVis = {
+    x: 100,
+    y: 10,
+    w: width - 100,
+    h: 300
+};
+
+var dataSet = {};
+
+var svg = d3.select("#vis").append("svg").attr({
+    width: width + margin.left + margin.right,
+    height: height + margin.top + margin.bottom
+}).append("g").attr({
+        transform: "translate(" + margin.left + "," + margin.top + ")"
+    });
+
+// --- this is just for fun.. play arround with it iof you like :)
+var projectionMethods = [
+    {
+        name:"mercator",
+        method: d3.geo.mercator().translate([width / 2, height / 2])//.precision(.1);
+    },{
+        name:"equiRect",
+        method: d3.geo.equirectangular().translate([width / 2, height / 2])//.precision(.1);
+    },{
+        name:"stereo",
+        method: d3.geo.stereographic().translate([width / 2, height / 2])//.precision(.1);
+    }
+];
+// --- this is just for fun.. play arround with it iof you like :)
+
+
+var actualProjectionMethod = 0;
+var colorMin = colorbrewer.Greens[3][0];
+var colorMax = colorbrewer.Greens[3][2];
+
+var newCountries = [];
+
+
+var path = d3.geo.path().projection(projectionMethods[0].method);
+
+/*
+
+var color = d3.scale.quantize()
+                    .range(["rgb(237,248,233)", "rgb(186,228,179)",
+                     "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)"]);
+
+                    */
+
+var color_domain = [50, 150, 350, 750, 1500];
+
+var color = d3.scale.quantize().range(colorbrewer.Greens[9]).domain(color_domain);
+
+
+
+var updateVis = function (data) {
+    var newDataCountry = {};
+    var arrays = [];
+    for (i in data){
+        arrays.push(data[i]["value"]);
+   // console.log(data[i]);
+
+
+            //Grab country name
+            var dataCountry = data[i]["country"]["id"];
+        //    console.log(dataCountry);
+
+             for (j in newCountries) {
+             //   console.log(j);
+               if(newCountries[j] == dataCountry){
+                    newDataCountry["key"]=  j;
+                    newDataCountry["value"] = data[i]["value"];
+               }
+            }
+
+
+};
+
+
+       
+ color.domain([
+                d3.min(data, function(d) { return parseFloat(d["value"]); }),
+                d3.max(data, function(d) { return parseFloat(d["value"]); })  ]);
+ 
+
+    svg.selectAll("path")
+    .data(data)
+   // .attr("class", function(d) { return color(rate.get(d["value"])); });
+        .style("fill", function(d) {
+        //    console.log(d);
+            if(d["value"]){
+                 var dataCountry = d["country"]["id"]
+                     for (j in newCountries) {
+                       if(newCountries[j] == dataCountry){
+                          var value = d["value"];
+                          console.log(value);
+                         return color(value);
+                        }
+                    }
+                }
+
+           else {
+                    return "#ccc";
+ 
+            }
+
+    });
+
+    
+};
+
+
+
+
+var initVis = function(error, indicators, world, countries){
+
+var country_name;
+var country_value;
+
+ var tooltip = d3.select("body")
+          .append("div")
+          .style("position", "absolute")
+          .style("z-index", "10")
+          .style("visibility", "hidden")
+          .style("color", "black")
+          .style("font-size", "15px")
+          .attr("class", "tooltip")
+    
+
+
+     svg.selectAll("path")
+        .data(world.features.filter(function(d) { return d.id != -99; }))
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("class", "country")
+        .on("mouseover", function(d){ console.log(d); tooltip.style("visibility", "visible");
+
+                    tooltip.text("You're currently mousing over " + d["country"]["value"] + " and it has an ID of " + d["country"]["id"] + 
+                        ". You are currently looking at the indicator for " + d["indicator"]["value"]+ " which has an ID of " +
+                        d["indicator"]["id"] + ". The shading for this country was determined using the value " + d["value"] +
+                        " because you selected the year " + d["date"] + "!"
+
+
+                        );
+
+            })
+                .on("mousemove", function(){return tooltip.style("top", (event.pageY-20)+"px").style("left",(event.pageX+20)+"px"); country_name = "hello";})
+                .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+        
+
+
+
+    var ind;
+    var year;
+ 
+ var indDrop = d3.select("#table_container")
+    .append("select")
+    .on("change", function() {
+      ind = this.options[this.selectedIndex].value;
+      //console.log(ind);
+    });
+
+
+var indicatorOpts = indDrop.selectAll("option")
+    .data(indicators)
+    .enter()
+    .append("option")
+      .text(function (d) { return d.IndicatorName; })
+      .attr("value", function (d) { return d.IndicatorCode; });
+
+
+counter = 1963;
+counter_end = 2013;
+years_array  = [];
+
+for (var i=0; i<=49; i++)
+{
+    counter++;
+    years_array.push(counter);
+}
+
+
+
+  var yearDrop = d3.select("#table_container2")
+    .append("select")
+    .on("change", function() { 
+      year = this.options[this.selectedIndex].value;
+    //  console.log(year);
+    });
+
+
+var yearOpts = yearDrop.selectAll("option")
+    .data(years_array)
+    .enter()
+    .append("option")
+      .text(function (d) { return d; })
+      .attr("value", function (d) { return d; });
+
+       countries.map(function(d) {
+    newCountries[d["Alpha-3 code"]] = d["Alpha-2 code"];
+  });
+
+      // console.log(newCountries);
+
+
+
+function runAQueryOn(ind,year) {
+   // console.log(ind);
+   // console.log(year);
+
+   console.log(ind);
+   console.log(year);
+
+    $.ajax({
+
+        url: "http://api.worldbank.org/countries/all/indicators/" + ind + "?format=jsonP&prefix=Getdata&per_page=500&date=" + (year),
+        jsonpCallback:'getdata',
+        dataType:'jsonp',
+        success: function (data, status){
+         //  console.log(data);
+           updateVis(data[1]);
+        }
+
+    });
+
+//updateVis();
+
+}
+
+var dind = "SP.DYN.CDRT.IN";
+var dyear = 2000;
+
+runAQueryOn(dind, dyear);
+
+
+var showButton = d3.select("body")
+    .append("button")
+    .text("Select All")
+    .on("click",function() {runAQueryOn(ind,year); });
+
+}
+
+// end of invit vis
+
+
+
+var legend_labels = ["Very Low", "Low", "Medium Low", "Medium", "High", "Really High"]
+var ext_color_domain = [0, 50, 150, 350, 750, 1500]
+
+  var legend = svg.selectAll("g.legend")
+  .data(ext_color_domain)
+  .enter().append("g")
+  .attr("class", "legend");
+
+  var ls_w = 20, ls_h = 20;
+
+  legend.append("rect")
+  .attr("x", 20)
+  .attr("y", function(d, i){ return height - (i*ls_h) - 2*ls_h;})
+  .attr("width", ls_w)
+  .attr("height", ls_h)
+  .style("fill", function(d, i) { return color(d); })
+  .style("opacity", 0.8);
+
+  legend.append("text")
+  .attr("x", 50)
+  .attr("y", function(d, i){ return height - (i*ls_h) - ls_h - 4;})
+  .text(function(d, i){ return legend_labels[i]; });
+
+
+
+
+
+
+// very cool queue function to make multiple calls.. 
+// see 
+queue()
+   // .defer(d3.csv,"../data/worldBank_indicators.csv")
+    .defer(d3.json,"../data/world_data.json")
+    //.defer(d3.json,"../data/WorldBankCountries.json")
+    .await(initVis);
+ 
+/*
+
+// just for fun 
+var textLabel = svg.append("text").text(projectionMethods[actualProjectionMethod].name).attr({
+    "transform":"translate(-40,-30)"
+})
+
+var changePro = function(){
+    actualProjectionMethod = (actualProjectionMethod+1) % (projectionMethods.length);
+
+    textLabel.text(projectionMethods[actualProjectionMethod].name);
+    path= d3.geo.path().projection(projectionMethods[actualProjectionMethod].method);
+    svg.selectAll(".country").transition().duration(750).attr("d",path);
+};
+
+d3.select("body").append("button").text("Select").on({
+    "click":changePro
+});
+
+
+d3.select("body").append("option").text("helllloooooo").on({
+    "click": 3
+});
+*/
