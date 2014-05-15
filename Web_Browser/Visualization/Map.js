9ea2a3af7eec9eaa5ca9067af404e3072fa41868
brandontineo <brandontineo@college.harvard.edu>
2014-05-14 20:17:55 -0400

    adding style

--- Web_Browser/Visualization/Map.js
+++ Web_Browser/Visualization/Map.js
@@ -949,7 +949,7 @@ for (i in merged_data)
 }
 
 
-all_keys.unshift("<Select a city>")
+all_keys.unshift("<Select a country>")
 
   var countryDrop = d3.select("#table_container1")
     .data(all_keys)
